71ee9eac43ef6aca31ac197ba327a4f9b2be391f
Vadim Belman <vrurg@lflat.org>
2018-03-30 22:24:56 -0400

    Support for origination data
    Adding attribute typing (isa + MooX::TypeTiny)

--- lib/ManulC/Object.pm
+++ lib/ManulC/Object.pm
@@ -2,16 +2,17 @@
 
 package ManulC::Object;
 
-our $VERSION = 'v0.001.001';
-
 use Devel::StackTrace;
 use Scalar::Util qw(blessed refaddr reftype weaken isweak);
 
 use ManulC::Exception;
-use ManulC::Util qw<:execControl>;
+use ManulC::Util qw<:execControl :data :namespace>;
+use Module::Loaded;
 
-use ManulC::Class;
+use ManulC::Class qw<allTypes>;
 classInit;
+
+our $VERSION = 'v0.001.001';
 
 # --- Internal attributes
 
@@ -19,6 +20,7 @@ has __id => (
     is      => 'ro',
     lazy    => 1,
     clearer => 1,
+    isa     => Str,
     default => sub {
         my $this  = shift;
         my $strID = ref( $this ) . '_' . refaddr( $this );
@@ -27,34 +29,126 @@ has __id => (
     },
 );
 
+has __orig_file  => ( is => 'rw', clearer => 1, );
+has __orig_line  => ( is => 'rw', clearer => 1, );
+has __orig_pkg   => ( is => 'rw', clearer => 1, );
+has __orig_sub   => ( is => 'rw', clearer => 1, );
+has __orig_stack => ( is => 'rw', clearer => 1, );
+
 # --- Public attributes
 # Main application object.
 has app => (
     is        => 'rwp',
+    isa       => InstanceOf ["ManulC::App"],
     predicate => 1,
     weak_ref  => 1,
 );
 
+# Boolean, true if DEBUG mode is on
 has DEBUG => (
     is      => 'rw',
+    isa     => Bool,
     lazy    => 1,
     clearer => 1,
+    coerce  => 1,
     builder => 'initDEBUG',
 );
 
+# Boolean, true if working under unit tests
 has TEST => (
     is      => 'rw',
+    isa     => Bool,
     lazy    => 1,
     clearer => 1,
+    coerce  => 1,
     builder => 'initTEST',
 );
 
+has STACKTRACE => (
+    is      => 'rw',
+    isa     => Bool,
+    coerce  => 1,
+    lazy    => 1,
+    clearer => 1,
+    builder => 'initSTACKTRACE',
+);
+
+# --- Public Methods
+
+sub BUILD {
+    my $this = shift;
+
+    if ( $this->DEBUG ) {
+        $this->__setup_origination;
+    }
+}
+
 sub initDEBUG {
-    return $ManulC::Util::DEBUG;
+    return !!$ManulC::Util::DEBUG;
 }
 
 sub initTEST {
-    return !!( $ENV{MANULC_TEST} // 0 );
+    return !!( $ENV{MANULC_TEST} // (is_loaded("ManulCTest") && $ManulCTest::TESTING) // 0 );
+}
+
+sub initSTACKTRACE {
+    my $this = shift;
+    # Don't do tracing if running under unit tests.
+    my $doTrace = !$this->TEST;
+    # The following won't change $doTrace if the env variable is not defined.
+    $doTrace = is_true( $ENV{MANULC_STACKTRACE} ) // $doTrace;
+    return $doTrace;
+}
+
+# --- Private Methods
+sub __setup_origination {
+    my $this = shift;
+    my ( $level ) = @_;
+
+    my @frame;
+    if ( defined $level ) {
+
+        # Skip our own frame.
+        $level++;
+    }
+    else {
+        @frame = caller( 1 );
+
+        # If called from BUILD then skip additional frame.
+        $level = $frame[3] =~ /::BUILD$/ ? 2 : 1;
+    }
+
+    my ( @foundFrame );
+    my $waitForNew = 1;
+    while ( @frame = caller( $level ) ) {
+        if ( $frame[3] =~ /::(?:create|new)$/ ) {
+            $waitForNew = 0;
+            @foundFrame = @frame;
+        }
+        else {
+            last unless $waitForNew;
+        }
+        $level++;
+    }
+
+    # Support static method call. Don't try to set object attributes if called as
+    # Optrade::Object->__orig or Optrade::Object::__orig.
+    if ( @foundFrame && ref( $this ) ) {
+        $this->__orig_pkg( $foundFrame[0] // '' );
+        $this->__orig_file( $foundFrame[1] );
+        $this->__orig_line( $foundFrame[2] );
+        $this->__orig_sub( $foundFrame[3] // '' );
+    }
+
+    my $frmCount = 0;
+    $this->__orig_stack(
+        Devel::StackTrace->new(
+            frame_filter => sub { $frmCount++ >= $level }
+        )
+      )
+      if ( $this->STACKTRACE );
+
+    return @foundFrame;
 }
 
 1;
